07f5ca8ebcf9931503e5db120ee51bc7d6daff06
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-27 10:21:11 +0200

    + Updated copyright info in license + Update license disclaimer

--- assets/Ocl/DataGrid/script.js
+++ assets/Ocl/DataGrid/script.js
@@ -1,3 +1,13 @@
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 var OTree = 
 {
     init : function()
--- src/TreeBox.php
+++ src/TreeBox.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Osynapsy package.
  *
- * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@ use Osynapsy\Html\Component;
 /**
  * Description of TreeBox
  *
- * @author Pietro Celeste <p.celeste@osynapsy.org>
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 class TreeBox extends Component
 {
