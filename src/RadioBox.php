da6b44854cb635bdb2f9d5b5e8546eae3735f8d9
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-31 10:48:44 +0200

    Refactoring RadioBox component

--- src/RadioBox.php
+++ src/RadioBox.php
@@ -11,23 +11,63 @@
 
 namespace Osynapsy\Ocl;
 
-class RadioBox extends InputBox
+use Osynapsy\Html\Component;
+use Osynapsy\Html\Tag;
+
+class RadioBox extends Component
 {
-    public function __construct($name, $id)
+    protected $label;
+    protected $radio;
+
+    public function __construct($id, $label, $value = null)
     {
-        parent::__construct('radio', $name, $id);
+        parent::__construct('span', $id.'_box');
+        $this->label = $label;
+        $this->radio = $this->radioFactory($id, $value);
+    }
+
+    protected function radioFactory($id, $value)
+    {
+        $radio = new Tag('input', $id);
+        $radio->att([
+            'type' => 'radio',
+            'name' => $id,
+            'value' => $value
+        ]);
+        return $radio;
     }
 
     public function __build_extra__()
     {
-        if ($this->value && strpos($this->name,'[')) {
-            list($name,) = explode('[',$this->name);
-            if (is_array($_REQUEST[$name]) && in_array($this->value, $_REQUEST[$name])) {
-                $this->att('checked','checked');
-            }
+        if ($this->isChecked()) {
+            $this->getRadio()->att('checked','checked');
         }
-        if (array_key_exists($this->name,$_REQUEST) && $_REQUEST[$this->name] == $this->value){
-            $this->att('checked','checked');
+        $this->add($this->getRadio());
+        if (!empty($this->label)) {
+            $this->add(' '.$this->label);
+        }
+    }
+
+    public function isChecked()
+    {
+        $radioName = $this->getRadio()->name;
+        $radioValue = $this->getRadio()->value;
+        if (strpos($radioName, '[') === false) {
+            return array_key_exists($radioName, $_REQUEST) && $_REQUEST[$radioName] == $radioValue ? true : false;
+        }
+        list($name,) = explode('[',$radioName);
+        return is_array($_REQUEST[$name]) && in_array($radioValue, $_REQUEST[$name]) ? true : false;
+    }
+
+    public function getRadio()
+    {
+        return $this->radio;
+    }
+
+    public function setDisabled($condition)
+    {
+        if ($condition) {
+            $this->getRadio()->att('disabled', 'disabled');
         }
     }
 }
